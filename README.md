26182f3b3d240ba699f162193d3b0cf4589b1ebe
Erik <enoex1@gmail.com>
2015-02-12 23:33:07 -0800

    updates readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# Pre-Attentive Processing
+
+Sources:
+    * http://tributary.io/inlet/7967f830240f6d5b4726
--- main.js
+++ main.js
@@ -7,7 +7,7 @@
  * ========================================================================= */
 // UTIL
 // --------------------------------------
-function testIntersection(rect1, rect2) {
+function testIntersection (rect1, rect2) {
     if (rect1.x < (rect2.x + rect2.width) && (rect1.x + rect1.width) > rect2.x &&
         rect1.y < (rect2.y + rect2.height) && (rect1.y + rect1.height) > rect2.y){
         return true;
@@ -220,7 +220,7 @@ d3.select('#button-flash-test1').on('click', function(d){
             d3.select('#test-1-shapes').style({ display: 'none' });
 
             runFlashTest1();
-        }, 84);
+        }, 4);
 
     });
 });
